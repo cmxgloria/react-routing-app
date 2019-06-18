5f7b2e2a9770352ed4df8deb8e36dffb95214368
Gloria Chen <cmxgloria@gmail.com>
2019-06-18 21:43:27 +1000

    added more Toute inside Switch

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ItemDetail from './ItemDetail';
 
 function App() {
   return (
     <Router>
+
       <div className='App'>
         <Nav />
-        <Route path='/about' component={About} />
-        <Route path='/shop' component={Shop} />
+        <Switch>
+          <Home path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/shop' exact component={Shop} />
+          <Route path='/shop/:id' component={ItemDetail} />
+        </Switch>
       </div>
-    </Router>
+
+    </Router >
   );
 }
-
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+)
 export default App;
