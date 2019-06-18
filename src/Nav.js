cfb1e47cbb846de4b2d3224327d3069ab8b81fd6
Gloria Chen <cmxgloria@gmail.com>
2019-06-18 21:42:09 +1000

    added Link to Nav component

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function Nav() {
+  const navStyle = {
+    color: 'white'
+  };
   return (
     <nav>
       <h3>Logo</h3>
       <ul className='nav-links'>
-        <li>About</li>
-        <li>Shop</li>
+        <Link style={navStyle} to='/about'><li>About</li>
+        </Link>
+        <Link style={navStyle} to='/shop'> <li>Shop</li></Link>
+
       </ul>
     </nav>
   );
