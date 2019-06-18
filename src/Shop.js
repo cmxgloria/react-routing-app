dc09437b74ca52d071417c524376abe8280e41d7
Gloria Chen <cmxgloria@gmail.com>
2019-06-18 21:41:41 +1000

    created shop component and use map method

--- src/Shop.js
+++ src/Shop.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import { Link } from 'react-router-dom';
 function Shop() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const response = await fetch('https://fortnite-api.theapinetwork.com/upcoming/get', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': '048bc666a548048036609cb410b1dd3a'
+      }
+    });
+    const result = await response.json();
+    console.log(result.data);
+    setItems(result.data);
+
+  }
   return (
     <div>
-      <h1>Shop Page</h1>
+      {items.map(item => (<h1 key={item.itemId}><Link to={`/shop/${item.itemId}`}>{item.item.name}</Link></h1>))}
     </div>
   );
 }
